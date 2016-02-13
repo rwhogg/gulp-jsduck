548ce932d5d64a5cd9025207edfa73e0edc6ba26
Bob W. Hogg <hogg.bob.2@gmail.com>
2016-02-13 12:35:36 -0800

    Depend on gulp-jscs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var GJSDuck = require("./index.js");
 var gjsduck = new GJSDuck(["--out", "docs"]);
+var jscs = require("gulp-jscs");
 var rimraf = require("rimraf").sync;
 
 gulp.task("test", function()
@@ -9,4 +10,11 @@ gulp.task("test", function()
     gulp.src("index.js")
         .pipe(gjsduck.doc());
     console.log("Go check to make sure it's good!");
+});
+
+gulp.task("lint", function()
+{
+    gulp.src("index.js")
+        .pipe(jscs())
+        .pipe(jscs.reporter());
 });
